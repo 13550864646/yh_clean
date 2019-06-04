4a50155f59bbe87bd3e0a0fbc266ed795fc5e2f3
13550864646@163.com <13550864646@163.com>
2019-06-04 17:00:35 +0800

    test

--- src/action/CleanAction.scala
+++ src/action/CleanAction.scala
@@ -3,7 +3,7 @@ package action
 import com.alibaba.fastjson.JSON
 import entity.JA.dt.dt_data
 import entity.TZ.TZdata
-import org.apache.spark.sql.{SQLContext, SaveMode}
+import org.apache.spark.sql.{SQLContext, SaveMode, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.json4s.native.Serialization.read
@@ -13,14 +13,14 @@ object CleanAction {
   def main(args: Array[String]): Unit = {
     val conf  = new SparkConf().setAppName("test").setMaster("local")
     val sc = new SparkContext(conf)
-    sc.hadoopConfiguration.set("fs.defaultFS", "hdfs://ha");
-    sc.hadoopConfiguration.set("dfs.nameservices", "ha");
-    sc.hadoopConfiguration.set("dfs.ha.namenodes.ha", "nn1,nn2");
-    sc.hadoopConfiguration.set("dfs.namenode.rpc-address.ha.nn1", "yunhe.bigdata-master.yhwx:8020");
-    sc.hadoopConfiguration.set("dfs.namenode.rpc-address.ha.nn2", "yunhe.bigdata-slave.yhwx:8020");
-    sc.hadoopConfiguration.set("dfs.client.failover.proxy.provider.ha", "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider");
+    sc.hadoopConfiguration.set("fs.defaultFS", "hdfs://ha")
+    sc.hadoopConfiguration.set("dfs.nameservices", "ha")
+    sc.hadoopConfiguration.set("dfs.ha.namenodes.ha", "nn1,nn2")
+    sc.hadoopConfiguration.set("dfs.namenode.rpc-address.ha.nn1", "yunhe.bigdata-master.yhwx:8020")
+    sc.hadoopConfiguration.set("dfs.namenode.rpc-address.ha.nn2", "yunhe.bigdata-slave.yhwx:8020")
+    sc.hadoopConfiguration.set("dfs.client.failover.proxy.provider.ha", "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider")
     val ssc = new StreamingContext(sc,Seconds(3))
-    val sql = new SQLContext(sc);
+    val sql = SparkSession.builder().config(conf).getOrCreate()
 
     /**
       *spark监听
@@ -60,7 +60,7 @@ object CleanAction {
     }
 
 //    百融借贷意向数据处理
-    val jddata = jd_stream.map{
+    jd_stream.map{
       line=>
         val jdyx = JSON.parseObject(line)
         val als_m12_cell_nbank_night_allnum = jdyx.get("als_m12_cell_nbank_night_allnum")
@@ -74,11 +74,10 @@ object CleanAction {
         val rs_scene = jdyx.get("rs_scene")
         val rs_product_name = jdyx.get("rs_product_name")
         (als_m12_cell_nbank_night_allnum,als_lst_cell_nbank_consnum,als_m12_id_nbank_cf_orgnum,als_m12_id_nbank_max_monnum,als_m12_cell_bank_tra_orgnum,als_lst_cell_nbank_inteday,rs_Rule_decision,rs_product_type,rs_scene,rs_product_name)
+    }.foreachRDD {
+      line =>
+        sql.createDataFrame(line).toDF().write.mode(SaveMode.Append).text("hdfs://ha/cusReport/resultdata/BR/jdyx")
     }
-//  .foreachRDD {
-//      line =>
-//        sql.createDataFrame(line).toDF().write.mode(SaveMode.Append).text("hdfs://ha/cusReport/resultdata/BR/jdyx")
-//    }
     //    百融特殊名单
     val tsdata = ts_stream.map{
       line=>
@@ -92,7 +91,7 @@ object CleanAction {
     }
 
 
-    jddata.print()
+//    jddata.print()
     ssc.start()
     ssc.awaitTermination()
   }
